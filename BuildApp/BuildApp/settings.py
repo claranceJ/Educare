a468387647f203caf2d84948bf1ba1b9fa9dfed4
claranceJ <jacktonec@gmail.com>
2024-11-11 11:09:50 +0300

    feat: Add and configure Vercel.json and requirements.txt

--- BuildApp/BuildApp/settings.py
+++ BuildApp/BuildApp/settings.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-3$h1950c8t35tlcg)(q#8v#gfoxrb$tn!h^gs5q@n=)@1=2kv=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
